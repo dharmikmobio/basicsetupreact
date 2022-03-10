8ae60ace14c5498384775eaf78f6b5134c78057e
Dharmik <dharmikp.mobio@gmail.com>
2022-03-10 11:38:09 +0530

    new class state

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
 export class App extends Component{
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+       hello:"hello world"
+    }
+  }
+  
   render(){
     return(
       <>
-            this is a class based file
+            <h1>{this.state.hello}</h1>
+            <button onClick={()=>this.setState({hello:"hello india"})}>Click to change class based</button>
       </>
     );
   }
