8a37c45a059f1e8c7a4b2a542dda44a87a46b5aa
Dharmik <dharmikp.mobio@gmail.com>
2022-03-10 15:10:44 +0530

    submit to show form data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react'
 
 function App() {
-    const [data,setData] = React.useState(null)
-
-    function datainput(val){
-        setData(val.target.value)
+    const [data, setData] = React.useState(null)
+    const [print, setPrint] = React.useState(false)
+   function inputData(val)  {
         console.log(val.target.value)
+        setData(val.target.value)
     }
   return (
     <>
-        <h1>{data}</h1>
-        <input type="text" onChange={datainput} />
+    <div>
+        {print?<h1>{data}</h1> : false }
+    </div>
+        
+        <input type="text" onChange={inputData} />
+        <button onClick={()=>setPrint(true)}>Click to submit</button>
     </>
   )
 }
