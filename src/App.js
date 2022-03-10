9fc50c77fd9dc4babd198deec41db0e5ea309cb3
Dharmik <dharmikp.mobio@gmail.com>
2022-03-10 15:20:49 +0530

    hide and show

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
 import React from 'react'
 
 function App() {
-    const [data, setData] = React.useState(null)
-    const [print, setPrint] = React.useState(false)
-   function inputData(val)  {
-        console.log(val.target.value)
-        setData(val.target.value)
-    }
+    const [data, setData] = React.useState(true)
   return (
     <>
-    <div>
-        {print?<h1>{data}</h1> : false }
-    </div>
+        <div>
+            {data? <h1>hello World</h1> : null }
+        </div>
         
-        <input type="text" onChange={inputData} />
-        <button onClick={()=>setPrint(true)}>Click to submit</button>
+        <button onClick={()=>setData(false)}>HIDE</button>
+        <button onClick={()=>setData(true)}>SHOW</button>
     </>
   )
 }
