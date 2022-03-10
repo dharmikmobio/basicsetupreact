481e42e2128d8d178303c7ed4647cc21c4a07aa9
Dharmik <dharmikp.mobio@gmail.com>
2022-03-10 10:55:25 +0530

    direct function called

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,6 +10,7 @@ function Header(){
            <p>Hello World from function component</p>
            <h1>Hello {data}</h1>
            <button onClick={Update}>click to change by function</button>
+           <button onClick={()=>alert("direct function called")}>click to change by function</button>
         </>
     );
 }
