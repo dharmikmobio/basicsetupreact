f3cecb9da1be5e036492668f5b7dde98ac4d6790
Dharmik <dharmikp.mobio@gmail.com>
2022-03-14 11:08:53 +0530

    form submitted

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
-import React from 'react'
+
+import React,{useState} from 'react'
 
 function App() {
-    const [data, setData] = React.useState(true)
+  const [name, setName] = useState("");
+  const [select, setSelect] = useState("");
+  const [check, setCheck] = useState(false);
+  function submitted(e){
+    console.warn(name,select,check);
+    e.preventDefault()
+  }
   return (
     <>
-        <div>
-            {
-                data? <h1>hello World</h1> : false
-            }
-        </div>
-        
-        <button onClick={() =>setData(!data)}> Toggle </button>
-        
+      <form onSubmit={submitted}>
+          <input type="text" placeholder='enter your name' onChange={(e)=>setName(e.target.value)} /><br/>
+          <select onChange={(e)=>setSelect(e.target.value)}><br/>
+            <option value="">select here</option><br/>
+            <option value="">100</option><br/>
+            <option value="">200</option><br/>
+            <option value="">300</option><br/>
+          </select><br/>
+          <input type="checkbox" onChange={(e)=>setCheck(e.target.checked)} /><br/>
+          <button type="submit">Submit</button>
+      </form>
     </>
   )
 }
